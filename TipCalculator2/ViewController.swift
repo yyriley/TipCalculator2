1608f07fbee73c973450f903ad6ba16fa8457f0a
yyriley <yyriley0124@gmail.com>
2021-05-13 23:22:24 -0500

    Removed alert

--- TipCalculator2/ViewController.swift
+++ TipCalculator2/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
   
   var billAmountDisplay: Int = 0
   var billAmount: Double = 0
+  var alertController: UIAlertController?
   
   @IBOutlet weak var billAmountTextField: UITextField!
   @IBOutlet weak var totalLabel: UILabel!
@@ -25,20 +26,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
   }
   
+  
+  //Format bill amount as entered by user
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
     if let digit = Int(string) {
       billAmountDisplay = billAmountDisplay * 10 + digit
-      
-      if billAmountDisplay > 1_000_000_000_00 {
-        let alert = UIAlertController(title: "Please enter amount less than 1 billion", message: nil, preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
-      
-        present(alert, animated: true, completion: nil)
-        billAmountTextField.text = ""
-      } else {
-        billAmountTextField.text = updateAmount()
-      }
+      billAmountTextField.text = updateAmount()
+    
     }
     if string == "" {
       billAmountDisplay = billAmountDisplay/10
@@ -60,18 +54,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     let tipPercentages = [0.15, 0.18, 0.2]
     let tip = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
     let total = billAmount + tip
-    
-    let formatter = NumberFormatter()
-    formatter.numberStyle = NumberFormatter.Style.currency
-    formatter.string(from: NSNumber(value:tip))
+  
     
     //Update Tip Amount Label
-    tipAmountLabel.text = String(format: "%.2f", formatter)
+    tipAmountLabel.text = String(format: "%.2f", tip)
     
     //Update total amount
     totalLabel.text = String(format: "%.2f", total)
     
   }
-  
 }
 
