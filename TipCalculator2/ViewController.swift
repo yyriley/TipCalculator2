c5857490f000d4519e7fb65572db769183214b28
yyriley <yyriley0124@gmail.com>
2021-05-14 01:06:35 -0500

    Added picker view for tip amounts

--- TipCalculator2/ViewController.swift
+++ TipCalculator2/ViewController.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate{
+class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource{
+  
+  
   
   var billAmountDisplay: Int = 0
   var billAmount: Double = 0
   var alertController: UIAlertController?
   
+  @IBOutlet weak var tipPickerView: UIPickerView!
   @IBOutlet weak var billAmountTextField: UITextField!
   @IBOutlet weak var totalLabel: UILabel!
-  @IBOutlet weak var tipControl: UISegmentedControl!
   @IBOutlet weak var tipAmountLabel: UILabel!
   
   override func viewDidLoad() {
@@ -23,6 +25,7 @@ class ViewController: UIViewController, UITextFieldDelegate{
     
     billAmountTextField.delegate = self
     billAmountTextField.placeholder = updateAmount()
+    tipPickerView.delegate = self
     
     initDisplay()
   }
@@ -33,12 +36,11 @@ class ViewController: UIViewController, UITextFieldDelegate{
     totalLabel.text = formatCurrency(0.00)
   }
   
-  //Format bill amount as entered by user
+  //Format bill amount as user enters it
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
     if let digit = Int(string) {
       billAmountDisplay = billAmountDisplay * 10 + digit
       billAmountTextField.text = updateAmount()
-    
     }
     if string == "" {
       billAmountDisplay = billAmountDisplay/10
@@ -53,10 +55,33 @@ class ViewController: UIViewController, UITextFieldDelegate{
     return formatCurrency(amount)
   }
 
-  @IBAction func calculateTip(_ sender: Any) {
+  
+  let tipAmounts = ["10%","15%","20%"]
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return tipAmounts[row]
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    tipAmounts.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    if (row == 0) {
+      calculateTip(0.10)
+    } else if (row == 1) {
+      calculateTip(0.15)
+    } else {
+        calculateTip(0.20)
+    }
+  }
+  
+  @IBAction func calculateTip(_ tipPercentage: Double) {
     //Get Total tip
-    let tipPercentages = [0.15, 0.18, 0.2]
-    let tip = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
+    let tip = billAmount * tipPercentage
     let total = billAmount + tip
   
     //Update Tip Amount Label
